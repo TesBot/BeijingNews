592d927d4472a84b6b49ca7ffefa4f15312941b4
Jhin <alexsjr@qq.com>
2017-05-14 22:01:03 +0800

    配置网络请求数据 使用xUtils3请求网络文本数据 用Gson解析json数据

--- app/src/main/java/app_beijingnews/alex/com/beijingnews/pager/NewsCenterPager.java
+++ app/src/main/java/app_beijingnews/alex/com/beijingnews/pager/NewsCenterPager.java
@@ -5,7 +5,15 @@ import android.graphics.Color;
 import android.view.Gravity;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
 import app_beijingnews.alex.com.beijingnews.base.BasePager;
+import app_beijingnews.alex.com.beijingnews.domain.NewsCenterPagerBean;
+import app_beijingnews.alex.com.beijingnews.utils.Constants;
 import app_beijingnews.alex.com.beijingnews.utils.LogUtil;
 
 /**
@@ -35,6 +43,63 @@ public class NewsCenterPager extends BasePager {
         //绑定数据
         textView.setText("新闻中心内容");
 
+        //联网请求数据
+        getDataFromNet();
+
+    }
+
+    /**
+     * 使用xUtils3联网请求数据
+     */
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(Constants.NEWCSCENTER_PAGER_URL);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                LogUtil.e("使用Xutils3联网请求成功 == "+result);
+                processData(result);
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                LogUtil.e("使用Xutils3联网请求失败 == "+ex.getMessage());
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                LogUtil.e("使用Xutils3联网请求 取消  == "+cex.getMessage());
+            }
+
+            @Override
+            public void onFinished() {
+                LogUtil.e("使用Xutils3联网请求完成 ");
+
+            }
+        });
+
+    }
+
+    /**
+     * 解析json数据和显示数据
+     * @param json
+     */
+    private void processData(String json) {
+        NewsCenterPagerBean bean = parsedJson(json);
+        String title = bean.getData().get(0).getChildren().get(1).getTitle();
+        LogUtil.e("使用Gson解析Json数据成功 title == "+title);
+    }
+
+    /**
+     * 解析json数据：1，使用系统的API解析 2，使用第三方框架解析json数据，如Gson，fastjson
+     * @param json
+     * @return
+     */
+    private NewsCenterPagerBean parsedJson(String json) {
+        Gson gson = new Gson();
+        NewsCenterPagerBean bean = gson.fromJson(json,NewsCenterPagerBean.class);
+        return bean;
     }
 }
 
---
+++ app/src/main/java/app_beijingnews/alex/com/beijingnews/utils/Constants.java
@@ -0,0 +1,21 @@
+package app_beijingnews.alex.com.beijingnews.utils;
+
+/**
+ * Created by Jhin on 2017/5/14 0014.
+ */
+
+public class Constants {
+
+    /**
+     * 联网请求的url
+     */
+    public static final String BASE_URL = "http://10.0.2.2:8080/web_home";
+
+    /**
+     * 新闻中心的网络地址
+     */
+    public static final String NEWCSCENTER_PAGER_URL = BASE_URL+"/static/api/news/categories.json";
+
+
+
+}
