0c25aba4b6b691ded6ea9544df1e4a9d7f2ddb23
Jhin <alexsjr@qq.com>
2017-05-25 21:59:52 +0800

    页签详情数据访问和解析 顶部新闻ViewPager数据的绑定&添加红点.

---
+++ app/src/main/java/app_beijingnews/alex/com/beijingnews/domain/TabDetailPagerBean.java
@@ -0,0 +1,294 @@
+package app_beijingnews.alex.com.beijingnews.domain;
+
+import java.util.List;
+
+/**
+ * Created by Jhin on 2017/5/25 0025.
+ * 页签页面的数据
+ */
+
+public class TabDetailPagerBean {
+
+
+    /**
+     * data : {"countcommenturl":"/client/content/countComment/","more":"/static/api/news/10007/list_2.json","news":[{"id":147314,"title":"寰掓鐧婚珮娆ｈ祻浜煄绗竴鐗囩孩鍙�","url":"/static/html/2015/10/20/714C6E50486D197B6F257C45.html","listimage":"/static/images/2015/10/20/3/1420927604KB07.jpg","pubdate":"2015-10-20 09:37","comment":true,"commenturl":"/client/user/newComment/147314","type":"news","commentlist":"/static/api/news/10012/14/147314/comment_1.json"},{"id":147313,"title":"閲嶅簡寮\u20ac閫氶鏉\u2032簰鑱旂綉鍏氦涓撶嚎","url":"/static/html/2015/10/20/704D6C52496C187A68227D43.html","listimage":"/static/images/2015/10/20/37/1420927604K49F.jpg","pubdate":"2015-10-20 09:31","comment":true,"commenturl":"/client/user/newComment/147313","type":"news","commentlist":"/static/api/news/10012/13/147313/comment_1.json"},{"id":147312,"title":"浼︽暒澶ф湰閽熺敱浜庣淮淇� 瑕侀潤闊�4涓湀","url":"/static/html/2015/10/20/764B68564F6A197B69237B44.html","listimage":"/static/images/2015/10/20/4/1420927604PBZA.jpg","pubdate":"2015-10-20 09:30","comment":true,"commenturl":"/client/user/newComment/147312","type":"news","commentlist":"/static/api/news/10012/12/147312/comment_1.json"},{"id":147267,"title":"鍒牱鍗楅潪锛氬緢閲庤洰浣嗕篃寰堢編涓�","url":"/static/html/2015/10/19/714C6759486D1D7E662B7C46.html","listimage":"/static/images/2015/10/19/84/14209276048ADH.jpg","pubdate":"2015-10-19 11:03","comment":true,"commenturl":"/client/user/newComment/147267","type":"news","commentlist":"/static/api/news/10012/67/147267/comment_1.json"},{"id":147266,"title":"鎶勫簳娓革細杩欎簺鍩庡競渚垮疁鍙堝ソ閫�","url":"/static/html/2015/10/19/724F6F514A6F10736C217F44.html","listimage":"/static/images/2015/10/19/78/1420927604L1EF.jpg","pubdate":"2015-10-19 10:59","comment":true,"commenturl":"/client/user/newComment/147266","type":"news","commentlist":"/static/api/news/10012/66/147266/comment_1.json"},{"id":147149,"title":"鍖椾含鍏氦寮\u20ac閫�5鍏冭祻绾㈡梾娓镐笓绾�","url":"/static/html/2015/10/16/714C68564C691A7A68277C48.html","listimage":"/static/images/2015/10/16/52/1420927604Y9DA.jpg","pubdate":"2015-10-16 10:44","comment":true,"commenturl":"/client/user/newComment/147149","type":"news","commentlist":"/static/api/news/10012/49/147149/comment_1.json"},{"id":147148,"title":"涔樺湴閾佷笉璺蜂簩閮庤吙 绠\u2033浜嗗悧锛�","url":"/static/html/2015/10/16/704D6759406519796C237D48.html","listimage":"/static/images/2015/10/16/31/1420927604GCA5.jpg","pubdate":"2015-10-16 10:42","comment":true,"commenturl":"/client/user/newComment/147148","type":"news","commentlist":"/static/api/news/10012/48/147148/comment_1.json"},{"id":147146,"title":"鑰嶅鐪嬭繃鏉ワ紒 鍥涘窛鏈\u20ac缇庢梾琛屾椂闂磋〃","url":"/static/html/2015/10/16/754866584C691D7D6C237843.html","listimage":"/static/images/2015/10/16/30/1420927604N7IG.jpg","pubdate":"2015-10-16 10:33","comment":true,"commenturl":"/client/user/newComment/147146","type":"news","commentlist":"/static/api/news/10012/46/147146/comment_1.json"},{"id":147124,"title":"娴峰矝姘戝鎴愯垷灞辨梾娓告柊浜偣","url":"/static/html/2015/10/16/704D66584E6B19796A237148.html","listimage":"/static/images/2015/10/16/54/1420927604BQXZ.jpg","pubdate":"2015-10-16 09:48","comment":true,"commenturl":"/client/user/newComment/147124","type":"news","commentlist":"/static/api/news/10012/24/147124/comment_1.json"},{"id":147123,"title":"瓒佺澶╄繕鍦紒璧剁揣鍘昏祻绉�","url":"/static/html/2015/10/16/764B68564A6F1B7B672E7B45.html","listimage":"/static/images/2015/10/16/42/1420927604BBFY.jpg","pubdate":"2015-10-16 09:42","comment":true,"commenturl":"/client/user/newComment/147123","type":"news","commentlist":"/static/api/news/10012/23/147123/comment_1.json"}],"title":"鍖椾含","topic":[],"topnews":[{"comment":true,"commentlist":"/static/api/news/10007/53/147253/comment_1.json","commenturl":"/client/user/newComment/147253","id":147253,"pubdate":"2015-10-19 07:18","title":"甯傛暀濮旓細涓珮鑰冭嫳璇瘯鍗风粨鏋勪笉鍙�","topimage":"/static/images/2015/10/19/36/1053274969EORV.jpg","type":"news","url":"/static/html/2015/10/19/714C6E504A6F1B7869277C42.html"},{"comment":true,"commentlist":"/static/api/news/10007/54/147254/comment_1.json","commenturl":"/client/user/newComment/147254","id":147254,"pubdate":"2015-10-19 07:22","title":"璀︽柟鎶湶鈥滀簩鐜崄涓夐儙鈥濆璁唴瀹�","topimage":"/static/images/2015/10/19/14/105235144895YS.jpg","type":"news","url":"/static/html/2015/10/19/724F6B554E6B1D7E68267C45.html"},{"id":147315,"title":"鐧惧勾褰瀵� 鈥滃浗淇濃\u20ac濆悐鑴氭ゼ","topimage":"/static/images/2015/10/20/80/467283565SGJ.jpg","url":"/static/html/2015/10/20/754868564D681E7C68227840.html","pubdate":"2015-10-20 09:57","comment":true,"commenturl":"/client/user/newComment/147315","type":"news","commentlist":"/static/api/news/10012/15/147315/comment_1.json"},{"id":147274,"title":"骞村害鏈\u20ac浣虫梾琛屽湴 璁╀綘鐮扮劧蹇冨姩","topimage":"/static/images/2015/10/19/70/467283565C7N.jpg","url":"/static/html/2015/10/19/704D6C524D681D7E6D217D44.html","pubdate":"2015-10-19 11:42","comment":true,"commenturl":"/client/user/newComment/147274","type":"news","commentlist":"/static/api/news/10012/74/147274/comment_1.json"}]}
+     * retcode : 200
+     */
+
+    private DataBean data;
+    private int retcode;
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public int getRetcode() {
+        return retcode;
+    }
+
+    public void setRetcode(int retcode) {
+        this.retcode = retcode;
+    }
+
+    public static class DataBean {
+        /**
+         * countcommenturl : /client/content/countComment/
+         * more : /static/api/news/10007/list_2.json
+         * news : [{"id":147314,"title":"寰掓鐧婚珮娆ｈ祻浜煄绗竴鐗囩孩鍙�","url":"/static/html/2015/10/20/714C6E50486D197B6F257C45.html","listimage":"/static/images/2015/10/20/3/1420927604KB07.jpg","pubdate":"2015-10-20 09:37","comment":true,"commenturl":"/client/user/newComment/147314","type":"news","commentlist":"/static/api/news/10012/14/147314/comment_1.json"},{"id":147313,"title":"閲嶅簡寮\u20ac閫氶鏉\u2032簰鑱旂綉鍏氦涓撶嚎","url":"/static/html/2015/10/20/704D6C52496C187A68227D43.html","listimage":"/static/images/2015/10/20/37/1420927604K49F.jpg","pubdate":"2015-10-20 09:31","comment":true,"commenturl":"/client/user/newComment/147313","type":"news","commentlist":"/static/api/news/10012/13/147313/comment_1.json"},{"id":147312,"title":"浼︽暒澶ф湰閽熺敱浜庣淮淇� 瑕侀潤闊�4涓湀","url":"/static/html/2015/10/20/764B68564F6A197B69237B44.html","listimage":"/static/images/2015/10/20/4/1420927604PBZA.jpg","pubdate":"2015-10-20 09:30","comment":true,"commenturl":"/client/user/newComment/147312","type":"news","commentlist":"/static/api/news/10012/12/147312/comment_1.json"},{"id":147267,"title":"鍒牱鍗楅潪锛氬緢閲庤洰浣嗕篃寰堢編涓�","url":"/static/html/2015/10/19/714C6759486D1D7E662B7C46.html","listimage":"/static/images/2015/10/19/84/14209276048ADH.jpg","pubdate":"2015-10-19 11:03","comment":true,"commenturl":"/client/user/newComment/147267","type":"news","commentlist":"/static/api/news/10012/67/147267/comment_1.json"},{"id":147266,"title":"鎶勫簳娓革細杩欎簺鍩庡競渚垮疁鍙堝ソ閫�","url":"/static/html/2015/10/19/724F6F514A6F10736C217F44.html","listimage":"/static/images/2015/10/19/78/1420927604L1EF.jpg","pubdate":"2015-10-19 10:59","comment":true,"commenturl":"/client/user/newComment/147266","type":"news","commentlist":"/static/api/news/10012/66/147266/comment_1.json"},{"id":147149,"title":"鍖椾含鍏氦寮\u20ac閫�5鍏冭祻绾㈡梾娓镐笓绾�","url":"/static/html/2015/10/16/714C68564C691A7A68277C48.html","listimage":"/static/images/2015/10/16/52/1420927604Y9DA.jpg","pubdate":"2015-10-16 10:44","comment":true,"commenturl":"/client/user/newComment/147149","type":"news","commentlist":"/static/api/news/10012/49/147149/comment_1.json"},{"id":147148,"title":"涔樺湴閾佷笉璺蜂簩閮庤吙 绠\u2033浜嗗悧锛�","url":"/static/html/2015/10/16/704D6759406519796C237D48.html","listimage":"/static/images/2015/10/16/31/1420927604GCA5.jpg","pubdate":"2015-10-16 10:42","comment":true,"commenturl":"/client/user/newComment/147148","type":"news","commentlist":"/static/api/news/10012/48/147148/comment_1.json"},{"id":147146,"title":"鑰嶅鐪嬭繃鏉ワ紒 鍥涘窛鏈\u20ac缇庢梾琛屾椂闂磋〃","url":"/static/html/2015/10/16/754866584C691D7D6C237843.html","listimage":"/static/images/2015/10/16/30/1420927604N7IG.jpg","pubdate":"2015-10-16 10:33","comment":true,"commenturl":"/client/user/newComment/147146","type":"news","commentlist":"/static/api/news/10012/46/147146/comment_1.json"},{"id":147124,"title":"娴峰矝姘戝鎴愯垷灞辨梾娓告柊浜偣","url":"/static/html/2015/10/16/704D66584E6B19796A237148.html","listimage":"/static/images/2015/10/16/54/1420927604BQXZ.jpg","pubdate":"2015-10-16 09:48","comment":true,"commenturl":"/client/user/newComment/147124","type":"news","commentlist":"/static/api/news/10012/24/147124/comment_1.json"},{"id":147123,"title":"瓒佺澶╄繕鍦紒璧剁揣鍘昏祻绉�","url":"/static/html/2015/10/16/764B68564A6F1B7B672E7B45.html","listimage":"/static/images/2015/10/16/42/1420927604BBFY.jpg","pubdate":"2015-10-16 09:42","comment":true,"commenturl":"/client/user/newComment/147123","type":"news","commentlist":"/static/api/news/10012/23/147123/comment_1.json"}]
+         * title : 鍖椾含
+         * topic : []
+         * topnews : [{"comment":true,"commentlist":"/static/api/news/10007/53/147253/comment_1.json","commenturl":"/client/user/newComment/147253","id":147253,"pubdate":"2015-10-19 07:18","title":"甯傛暀濮旓細涓珮鑰冭嫳璇瘯鍗风粨鏋勪笉鍙�","topimage":"/static/images/2015/10/19/36/1053274969EORV.jpg","type":"news","url":"/static/html/2015/10/19/714C6E504A6F1B7869277C42.html"},{"comment":true,"commentlist":"/static/api/news/10007/54/147254/comment_1.json","commenturl":"/client/user/newComment/147254","id":147254,"pubdate":"2015-10-19 07:22","title":"璀︽柟鎶湶鈥滀簩鐜崄涓夐儙鈥濆璁唴瀹�","topimage":"/static/images/2015/10/19/14/105235144895YS.jpg","type":"news","url":"/static/html/2015/10/19/724F6B554E6B1D7E68267C45.html"},{"id":147315,"title":"鐧惧勾褰瀵� 鈥滃浗淇濃\u20ac濆悐鑴氭ゼ","topimage":"/static/images/2015/10/20/80/467283565SGJ.jpg","url":"/static/html/2015/10/20/754868564D681E7C68227840.html","pubdate":"2015-10-20 09:57","comment":true,"commenturl":"/client/user/newComment/147315","type":"news","commentlist":"/static/api/news/10012/15/147315/comment_1.json"},{"id":147274,"title":"骞村害鏈\u20ac浣虫梾琛屽湴 璁╀綘鐮扮劧蹇冨姩","topimage":"/static/images/2015/10/19/70/467283565C7N.jpg","url":"/static/html/2015/10/19/704D6C524D681D7E6D217D44.html","pubdate":"2015-10-19 11:42","comment":true,"commenturl":"/client/user/newComment/147274","type":"news","commentlist":"/static/api/news/10012/74/147274/comment_1.json"}]
+         */
+
+        private String countcommenturl;
+        private String more;
+        private String title;
+        private List<NewsBean> news;
+        private List<?> topic;
+        private List<TopnewsBean> topnews;
+
+        public String getCountcommenturl() {
+            return countcommenturl;
+        }
+
+        public void setCountcommenturl(String countcommenturl) {
+            this.countcommenturl = countcommenturl;
+        }
+
+        public String getMore() {
+            return more;
+        }
+
+        public void setMore(String more) {
+            this.more = more;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<NewsBean> getNews() {
+            return news;
+        }
+
+        public void setNews(List<NewsBean> news) {
+            this.news = news;
+        }
+
+        public List<?> getTopic() {
+            return topic;
+        }
+
+        public void setTopic(List<?> topic) {
+            this.topic = topic;
+        }
+
+        public List<TopnewsBean> getTopnews() {
+            return topnews;
+        }
+
+        public void setTopnews(List<TopnewsBean> topnews) {
+            this.topnews = topnews;
+        }
+
+        public static class NewsBean {
+            /**
+             * id : 147314
+             * title : 寰掓鐧婚珮娆ｈ祻浜煄绗竴鐗囩孩鍙�
+             * url : /static/html/2015/10/20/714C6E50486D197B6F257C45.html
+             * listimage : /static/images/2015/10/20/3/1420927604KB07.jpg
+             * pubdate : 2015-10-20 09:37
+             * comment : true
+             * commenturl : /client/user/newComment/147314
+             * type : news
+             * commentlist : /static/api/news/10012/14/147314/comment_1.json
+             */
+
+            private int id;
+            private String title;
+            private String url;
+            private String listimage;
+            private String pubdate;
+            private boolean comment;
+            private String commenturl;
+            private String type;
+            private String commentlist;
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public String getListimage() {
+                return listimage;
+            }
+
+            public void setListimage(String listimage) {
+                this.listimage = listimage;
+            }
+
+            public String getPubdate() {
+                return pubdate;
+            }
+
+            public void setPubdate(String pubdate) {
+                this.pubdate = pubdate;
+            }
+
+            public boolean isComment() {
+                return comment;
+            }
+
+            public void setComment(boolean comment) {
+                this.comment = comment;
+            }
+
+            public String getCommenturl() {
+                return commenturl;
+            }
+
+            public void setCommenturl(String commenturl) {
+                this.commenturl = commenturl;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public String getCommentlist() {
+                return commentlist;
+            }
+
+            public void setCommentlist(String commentlist) {
+                this.commentlist = commentlist;
+            }
+        }
+
+        public static class TopnewsBean {
+            /**
+             * comment : true
+             * commentlist : /static/api/news/10007/53/147253/comment_1.json
+             * commenturl : /client/user/newComment/147253
+             * id : 147253
+             * pubdate : 2015-10-19 07:18
+             * title : 甯傛暀濮旓細涓珮鑰冭嫳璇瘯鍗风粨鏋勪笉鍙�
+             * topimage : /static/images/2015/10/19/36/1053274969EORV.jpg
+             * type : news
+             * url : /static/html/2015/10/19/714C6E504A6F1B7869277C42.html
+             */
+
+            private boolean comment;
+            private String commentlist;
+            private String commenturl;
+            private int id;
+            private String pubdate;
+            private String title;
+            private String topimage;
+            private String type;
+            private String url;
+
+            public boolean isComment() {
+                return comment;
+            }
+
+            public void setComment(boolean comment) {
+                this.comment = comment;
+            }
+
+            public String getCommentlist() {
+                return commentlist;
+            }
+
+            public void setCommentlist(String commentlist) {
+                this.commentlist = commentlist;
+            }
+
+            public String getCommenturl() {
+                return commenturl;
+            }
+
+            public void setCommenturl(String commenturl) {
+                this.commenturl = commenturl;
+            }
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getPubdate() {
+                return pubdate;
+            }
+
+            public void setPubdate(String pubdate) {
+                this.pubdate = pubdate;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getTopimage() {
+                return topimage;
+            }
+
+            public void setTopimage(String topimage) {
+                this.topimage = topimage;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+        }
+    }
+}
--- app/src/main/java/app_beijingnews/alex/com/beijingnews/menudetailpager/tabledetailpager/TableDetailPager.java
+++ app/src/main/java/app_beijingnews/alex/com/beijingnews/menudetailpager/tabledetailpager/TableDetailPager.java
@@ -2,13 +2,31 @@ package app_beijingnews.alex.com.beijingnews.menudetailpager.tabledetailpager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import org.xutils.common.Callback;
+import org.xutils.common.util.DensityUtil;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import java.util.List;
 
 import app_beijingnews.alex.com.beijingnews.R;
 import app_beijingnews.alex.com.beijingnews.base.MenuDetailBasePager;
 import app_beijingnews.alex.com.beijingnews.domain.NewsCenterPagerBean2;
+import app_beijingnews.alex.com.beijingnews.domain.TabDetailPagerBean;
+import app_beijingnews.alex.com.beijingnews.utils.CacheUtils;
 import app_beijingnews.alex.com.beijingnews.utils.Constants;
 import app_beijingnews.alex.com.beijingnews.utils.LogUtil;
 
@@ -19,8 +37,17 @@ import app_beijingnews.alex.com.beijingnews.utils.LogUtil;
 
 public class TableDetailPager extends MenuDetailBasePager {
 
+    private ViewPager viewpager;
+    private TextView tv_title;
+    private LinearLayout ll_point_group;
+    private ListView listview;
+
     private final NewsCenterPagerBean2.DetailPagerData.ChildrenData childrenData;
     private String url;
+    /**
+     * 顶部轮播图部分数据
+     */
+    private List<TabDetailPagerBean.DataBean.TopnewsBean> topnews;
 
     public TableDetailPager(Context context, NewsCenterPagerBean2.DetailPagerData.ChildrenData childrenData) {
         super(context);
@@ -32,6 +59,11 @@ public class TableDetailPager extends MenuDetailBasePager {
     @Override
     public View initView() {
         View view = View.inflate(context, R.layout.tabdetail_pager,null);
+        viewpager = (ViewPager) view.findViewById(R.id.viewpager);
+        tv_title = (TextView) view.findViewById(R.id.tv_title);
+        ll_point_group = (LinearLayout) view.findViewById(R.id.ll_point_group);
+        listview = (ListView) view.findViewById(R.id.listview);
+
         return view;
     }
 
@@ -39,6 +71,167 @@ public class TableDetailPager extends MenuDetailBasePager {
     public void initData() {
         super.initData();
         url = Constants.BASE_URL + childrenData.getUrl();
-        LogUtil.e(childrenData.getTitle()+" 的联网地址： "+url);
+
+        //把缓存的数据取出来
+        String saveJson = CacheUtils.getString(context,url);
+        if(!TextUtils.isEmpty(saveJson)){
+            //解析数据
+            processData(saveJson);
+        }
+
+//        LogUtil.e(childrenData.getTitle()+" 的联网地址： "+url);
+
+        //联网请求数据
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams(url);
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                LogUtil.e(childrenData.getTitle()+"- 页面数据请求成功 == "+result);
+
+                //缓存数据
+                CacheUtils.putString(context,url,result);
+
+                //解析和处理数据
+                processData(result);
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                LogUtil.e(childrenData.getTitle()+"- 页面数据请求失败 == "+ex);
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                LogUtil.e(childrenData.getTitle()+"- 页面数据请求onCancelled == "+cex.getMessage());
+
+            }
+
+            @Override
+            public void onFinished() {
+                LogUtil.e(childrenData.getTitle()+"- 页面数据请求onFinished == ");
+
+
+            }
+        });
+    }
+
+    /**
+     * 之前点 高亮的位置
+     */
+    private int prePosition;
+
+    private void processData(String json) {
+        TabDetailPagerBean bean = parsedJson(json);
+        LogUtil.e(childrenData.getTitle()+"- 解析成功 == "+bean.getData().getNews().get(0).getTitle());
+
+        //顶部轮播图
+        topnews = bean.getData().getTopnews();
+
+        //设置viewpager适配器
+        viewpager.setAdapter(new TabDetailPagerTopNewsAdapter());
+
+        ll_point_group.removeAllViews();//移除所有的红点
+
+        //按照多少数据去添加点
+        for(int i=0;i<topnews.size();i++){
+            ImageView imageView = new ImageView(context);
+            //点 选择器
+            imageView.setBackgroundResource(R.drawable.point_selector);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(DensityUtil.dip2px(8),DensityUtil.dip2px(8));
+
+
+            if(i==0){
+                imageView.setEnabled(true);
+            }else{
+                imageView.setEnabled(false);
+                params.leftMargin = DensityUtil.dip2px(8);
+            }
+            imageView.setLayoutParams(params);
+            ll_point_group.addView(imageView);
+
+        }
+
+        //监听页面的改变，设置红点变化，文本变化
+
+        viewpager.addOnPageChangeListener(new MyOnPageChangeListener());
+        tv_title.setText(topnews.get(prePosition).getTitle());
+
+    }
+
+    class MyOnPageChangeListener implements ViewPager.OnPageChangeListener{
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            //1. 设置文本
+            tv_title.setText(topnews.get(position).getTitle());
+            //2. 对应页面红点高亮
+                //把之前的变成灰色
+            ll_point_group.getChildAt(prePosition).setEnabled(false);
+                //把当前的红色
+            ll_point_group.getChildAt(position).setEnabled(true);
+
+            prePosition = position;
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+
+    class TabDetailPagerTopNewsAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return topnews.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            //设置默认图片
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(R.drawable.home_scroll_default);
+            //XY拉伸
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            //图片加到容器中
+            container.addView(imageView);
+
+            TabDetailPagerBean.DataBean.TopnewsBean topnewsData = topnews.get(position);
+            String imageUrl = Constants.BASE_URL + topnewsData.getTopimage();
+
+            //联网请求图片
+            x.image().bind(imageView,imageUrl);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+//            super.destroyItem(container, position, object);
+            container.removeView((View) object);
+        }
+
+    }
+
+    private TabDetailPagerBean parsedJson(String json) {
+        return new Gson().fromJson(json,TabDetailPagerBean.class);
     }
 }
